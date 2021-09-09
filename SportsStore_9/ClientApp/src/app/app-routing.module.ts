5437f355fe22f9f7a43298f3ff2903ea586eed11
nguyen y <mnihon3@gmail.com>
2021-09-09 12:46:04 +0700

    add code first time

---
+++ SportsStore_9/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+// import { ProductTableComponent } from "./structure/productTable.component"
+// import { ProductDetailComponent } from "./structure/productDetail.component";
+import { ProductSelectionComponent } from "./store/productSelection.component";
+import { CartDetailComponent } from "./store/cartDetail.component";
+
+import { CheckoutDetailsComponent }
+from "./store/checkout/checkoutDetails.component";
+import { CheckoutPaymentComponent }
+from "./store/checkout/checkoutPayment.component";
+import { CheckoutSummaryComponent }
+from "./store/checkout/checkoutSummary.component";
+import { OrderConfirmationComponent }
+from "./store/checkout/orderConfirmation.component";
+
+const routes: Routes = [
+  {
+    path: "admin",
+    loadChildren: () =>
+    import("./admin/admin.module").then(module => module.AdminModule),
+  },
+  { path: "checkout/step1", component: CheckoutDetailsComponent },
+  { path: "checkout/step2", component: CheckoutPaymentComponent },
+  { path: "checkout/step3", component: CheckoutSummaryComponent },
+  { path: "checkout/confirmation", component: OrderConfirmationComponent },
+  { path: "checkout", redirectTo: "/checkout/step1", pathMatch: "full" },
+  
+    { path: "cart", component: CartDetailComponent },
+    // { path: "table", component: ProductTableComponent },
+    // { path: "detail", component: ProductDetailComponent },
+    // { path: "detail/:id", component: ProductDetailComponent },    
+    // { path: "", component: ProductTableComponent }
+    { path: "store/:category", component: ProductSelectionComponent },
+    { path: "store", component: ProductSelectionComponent },
+    { path: "", redirectTo: "/store", pathMatch: "full" }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ SportsStore_9/ServerApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServerApp.Models;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+namespace ServerApp.Controllers {
+public class HomeController : Controller {
+private DataContext context;
+private readonly ILogger _logger;
+public HomeController(DataContext ctx,ILogger<HomeController> logger) {
+context = ctx;
+_logger = logger;
+}
+public IActionResult Index() {
+    //_logger.LogInformation(22,"Log message in the Index() method");
+    this._logger.LogInformation("hahahahahahdfdfdfdfdfdd");
+    //System.Console.Write("taissao");
+
+return View(context.Products.First());
+}
+public IActionResult Privacy() {
+return View();
+}
+[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+NoStore = true)]
+public IActionResult Error() {
+return View(new ErrorViewModel { RequestId = Activity.Current?.Id
+?? HttpContext.TraceIdentifier });
+}
+}
+}
