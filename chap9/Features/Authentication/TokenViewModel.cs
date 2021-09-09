203b4caa0aeaa15bd30a55b4bd96bce169e8a071
nguyen y <mnihon3@gmail.com>
2021-09-09 14:32:10 +0700

    add code vue

---
+++ chap9/Features/Authentication/TokenViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+namespace ECommerce.Features.Authentication
+{
+public class TokenViewModel
+{
+[JsonProperty("access_token")]
+public string AccessToken { get; set; }
+[JsonProperty("access_token_expiration")]
+public DateTime AccessTokenExpiration { get; set; }
+public string FirstName { get; set; }
+public string LastName { get; set; }
+public IEnumerable<string> Roles { get; set; }
+}
+}
